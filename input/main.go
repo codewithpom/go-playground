fa3944ffdbff11a9297341982f8732d123898efb
Padmashree Jha <codewithpom@gmail.com>
2021-11-27 16:15:09 +0530

    Done input

---
+++ input/main.go
@@ -0,0 +1,11 @@
+// In this file we will learn how to take inputs from standard input and print them to standard output.
+
+package main
+
+import "fmt"
+
+func main() {
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println(input)
+}
